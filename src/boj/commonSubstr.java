f386b103da3f4e22ef7312a003c9837e63efeced
see101702 <see101702@gmail.com>
2026-01-04 15:30:19 +0900

    공통부분문자열

---
+++ src/boj/commonSubstr.java
@@ -0,0 +1,31 @@
+package boj;
+
+import java.io.*;
+
+//5582
+public class commonSubstr {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = bf.readLine();
+        String str2 = bf.readLine();
+
+        int n = str1.length();
+        int m = str2.length();
+
+        int[][] dp = new int[n+1][m+1];
+        int max = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str1.charAt(i-1) == str2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    max = Math.max(max, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
